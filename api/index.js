639f866ad4d96348425da4155b8c8839ca2f08cb
Apoorva Rao <apoorvarao147@gmail.com>
2024-02-29 11:37:34 -0500

    create user update api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js"
 import authRouter from "./routes/auth.route.js"
+import cookieParser from "cookie-parser"
 dotenv.config();
 
 mongoose
@@ -17,6 +18,8 @@ mongoose
 const app = express();
 
 app.use(express.json())
+
+app.use(cookieParser())
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter)
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,10 @@
 import express from "express";
-import {test} from '../controllers/user.controller.js'
+import { test, updateUser } from '../controllers/user.controller.js'
+import { verifyToken } from "../utils/verifyUser.js";
 
 const userRouter = express.Router();
 
 userRouter.get("/test", test);
+userRouter.post("/update/:id",verifyToken, updateUser)
 
 export default userRouter;
