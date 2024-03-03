a3b94f7eab57a5c7cf65d9c45030923125e321af
Apoorva Rao <apoorvarao147@gmail.com>
2024-03-02 20:49:21 -0500

    create update listing api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   createListing,
   deleteListing,
+  updateListing,
 } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -9,5 +10,6 @@ const listingRouter = express.Router();
 
 listingRouter.post("/create", verifyToken, createListing);
 listingRouter.delete("/delete/:id", verifyToken, deleteListing);
+listingRouter.post("/update/:id", verifyToken, updateListing);
 
 export default listingRouter;
