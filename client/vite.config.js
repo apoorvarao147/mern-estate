536fb8a0e1835db790d921b50a89ae7610a03967
Apoorva Rao <apoorvarao147@gmail.com>
2024-03-06 17:46:21 -0500

    change server

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api' : {
-        target: 'http://localhost:3000',
+        target: 'http://3.137.138.241:3000/',
         secure: false,
       },
     },
