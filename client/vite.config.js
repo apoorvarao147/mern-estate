e67ccfa86036bf0235179d4c2a54900acbd49e44
Apoorva Rao <apoorvarao147@gmail.com>
2024-03-09 08:40:22 -0500

    change proxy target

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api' : {
-        target: 'https://mernapi.apoorvarao.com',
+        target: 'http://localhost:3000',
         secure: false,
       },
     },
